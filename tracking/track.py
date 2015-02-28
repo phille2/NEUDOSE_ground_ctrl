fe1fe633abfef579240e06c4e096c9d3cd81a99a
Jim Rustle <jim_rustle@live.com>
2015-02-28 18:03:43 -0500

    Create PyEphem framework

---
+++ tracking/track.py
@@ -0,0 +1,10 @@
+import tracker
+
+def main():
+    T = tracker.Tracker()
+    #T._print_debug_get_info_object()
+    print T.calculate_current_position()
+
+if __name__ == '__main__':
+    main()
+
